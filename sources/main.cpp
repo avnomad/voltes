b0eddf12ff94e3591f3b597ade6bd3d7d1f78fa4
Vaptistis Anogeianakis <gd.nightmare@gmail.com>
2012-02-14 03:55:40 +0200

    Committed unmodified code from "unreadable 2" implementation.
    Original timestamps:
    main.cpp:              14/5/2009  1:08:50am
    count paths.h:         11/5/2009 11:07:03pm
    count paths.cpp:       12/5/2009  8:59:47pm
    
    --HG--
    branch : 11 - unreadable 2

--- sources/main.cpp
+++ sources/main.cpp
@@ -34,7 +34,7 @@ int main(int argc , char **argv)
 {
 	ifstream in;
 #pragma region open output stream
-	ofstream out("c:/output/stats unreadable.txt");
+	ofstream out("c:/output/stats unreadable 2.txt");
 	if(!out)
 	{
 		cerr << "cannot open output file!\n";
@@ -45,7 +45,7 @@ int main(int argc , char **argv)
 	uint N,M,T;
 	uint startI,startJ,stopI,stopJ;
 	uint K;
-	uint tempI,tempJ;
+	uint tempI,tempJ,level;
 	uint *table;
 	uint *p,*q,*sp,*sq;
 	uint distanceBase,distance;
@@ -68,68 +68,85 @@ int main(int argc , char **argv)
 		#pragma region read from input
 		in >> N >> M >> T >> startI >> startJ >> stopI >> stopJ >> K;	// read parameters from input.
 		#pragma endregion
-		#pragma region create the table
+		#pragma region create the table and border
 		N += 2 , M += 2;	// adjust the number of rows and columns to accommodate the border.
 		D = T+1;
 		table = new uint[N*M*D];	// allocate space for the table.
 
-		tempI = M*D;
-		tempJ = tempI-D;
-
-		distanceBase = 0;
-		for(p = table+tempI*stopI ; p > table ; p -= tempI)
+		level = M*D;
+		p = table;	// initialize p
+		sp = table + level;	// ps just after the end of the first row
+		while(p != sp)	// initialize the upper border
 		{
-			distance = distanceBase;
-			for(q = p + stopJ*D ; q > p ; q -= D)
-				*q = distance++;
-			distance = ++distanceBase;
-			sq = p + tempJ;
-			for(q = p + stopJ*D+D ; q < sq ; q += D)
-				*q = distance++;
-		} // end p for
+			*p = UINT_MAX;
+			p += D;
+		} // end while
 
-		distanceBase = 1;
-		sp = table+(N-1)*tempI;
-		for(p = table+tempI*stopI+tempI ; p < sp ; p += tempI)
+		distanceBase = stopI + stopJ - 2;	// the Manhattan distance between (1,1) and target
+		sp = table + stopI*level;	// first line stop
+		q = p + stopJ*D;	// first column stop
+		sq = p + level-D;	// second column stop
+		while(p != sp)
 		{
+			*p = UINT_MAX;	// start the line with border
+			p += D;
+
 			distance = distanceBase;
-			for(q = p + stopJ*D ; q > p ; q -= D)
-				*q = distance++;
-			distance = ++distanceBase;
-			sq = p + tempJ;
-			for(q = p + stopJ*D+D ; q < sq ; q += D)
-				*q = distance++;
-		} // end p for
+			while(p != q)
+			{
+				*p = distance--;
+				p += D;
+			} // end while
+			while(p != sq)
+			{
+				*p = distance++;
+				p += D;
+			} // end while
+			q += level;	// move column stops to the next row;
+			sq += level;
+
+			*p = UINT_MAX;	// end the line with border
+			p += D;
+			--distanceBase;	// decrement distanceBase for the next line
+		} // end while
+
+		sp = table + N*level - level;	// second line stop
+		while(p != sp)
+		{
+			*p = UINT_MAX;	// start the line with border
+			p += D;
+
+			distance = distanceBase;
+			while(p != q)
+			{
+				*p = distance--;
+				p += D;
+			} // end while
+			while(p != sq)
+			{
+				*p = distance++;
+				p += D;
+			} // end while
+			q += level;
+			sq += level;
+
+			*p = UINT_MAX;	// end the line with border
+			p += D;
+			++distanceBase;	// decrement distanceBase for the next line
+		} // end while
+
+		sp = p + level;	// ps one row below p (this should be just after the end of table
+		while(p != sp)	// initialize the lower border
+		{
+			*p = UINT_MAX;
+			p += D;
+		} // end while
 		#pragma endregion
 		#pragma region read obstacles
 		while(K--)
 		{
 			in >> tempI >> tempJ;	// read obstacle coordinates.
 			*(table + (M*tempI + tempJ)*D) = UINT_MAX;	// register obstacle
-		} // end while
-		#pragma endregion
-		#pragma region generate border
-		// generate border
-		K = M;
-		p = table;
-		while(K--)
-		{
-			*p = UINT_MAX;
-			p += D;
-		} // end while
-		K = N-1;
-		while(--K)
-		{
-			*p = UINT_MAX;
-			p += (M-1)*D;
-			*p = UINT_MAX;
-			p += D;
-		} // end while
-		K = M;
-		while(K--)
-		{
-			*p = UINT_MAX;
-			p += D;
 		} // end while
 		#pragma endregion
 		/*t = CPUclock::currentTime() - t;
@@ -152,7 +169,7 @@ int main(int argc , char **argv)
 		cout << t << CPUclock::getUnit() << '\t';
 		t = CPUclock::currentTime();*/
 		#pragma region count paths
-		uint temp = count_paths(table,M,startI,startJ,stopI,stopJ,T);
+		int temp = count_paths(table,M,startI,startJ,stopI,stopJ,T);
 		#pragma endregion
 		t = CPUclock::currentTime() - t;
 		cout << t << CPUclock::getUnit() << '\n';
