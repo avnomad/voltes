a0b90f5d436851e8b4963617b90b2a7f93b90a3d
Vaptistis Anogeianakis <gd.nightmare@gmail.com>
2012-02-14 03:37:47 +0200

    Committed unmodified code from "simple" implementation.
    Original timestamps:
    main.cpp:               8/5/2009  4:36:50pm
    count paths.h:          6/5/2009  6:50:26pm
    count paths.cpp:        6/5/2009 10:34:30pm
    
    --HG--
    branch : 2 - simple

--- sources/count paths.cpp
+++ sources/count paths.cpp
@@ -1,19 +1,45 @@
 #include "count paths.h"
 #include "cstdlib"
 
+// shared variables
+static bool *map;
+static uint columns;
+static uint ti,tj;
 
 static uint search4way(uint pi , uint pj , uint steps);															// search4way
 
 uint count_paths(bool *map , uint columns , uint pi , uint pj , uint ti , uint tj , uint steps)					// count_paths
 {
 	// look for trivial instance of the problem
-	if(*(map+pi*columns+pj) || steps < abs(pi-ti) + abs(pj-tj))
+	if(*(map+pi*columns+pj))
+		return 0;
+
+	unsigned int D = abs(pi-ti) + abs(pj-tj);
+	if(D > steps || (steps-D) & 0x1)
 		return 0;
 	
+	// initialize global variables
+	::map = map;
+	::columns = columns;
+	::ti = ti;
+	::tj = tj;
+
 	// commence search
 	if(steps--)
-		return count_paths(map,columns,pi+1,pj,ti,tj,steps) + count_paths(map,columns,pi,pj+1,ti,tj,steps) + 
-			count_paths(map,columns,pi-1,pj,ti,tj,steps) + count_paths(map,columns,pi,pj-1,ti,tj,steps);
+		return search4way(pi+1,pj,steps) + search4way(pi,pj+1,steps) + 
+			search4way(pi-1,pj,steps) + search4way(pi,pj-1,steps);
 	else
 		return (pi == ti && pj == tj)?1:0;
 } // end function count_paths
+
+
+static uint search4way(uint pi , uint pj , uint steps)															// search4way
+{
+	if(*(map+pi*columns+pj) || steps < abs(pi-ti) + abs(pj-tj))
+		return 0;
+	if(steps--)
+		return search4way(pi+1,pj,steps) + search4way(pi,pj+1,steps) + 
+			search4way(pi-1,pj,steps) + search4way(pi,pj-1,steps);
+	else
+		return (pi == ti && pj == tj)?1:0;
+} // end function search4way
