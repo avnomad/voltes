446795dffcc6d8dbe86716164ea01165317ca4c6
Vaptistis Anogeianakis <gd.nightmare@gmail.com>
2012-02-14 03:48:15 +0200

    Committed unmodified code from "no new" implementation.
    Original timestamps:
    main.cpp:              12/5/2009  3:19:10pm
    count paths.h:         11/5/2009 11:07:03pm
    count paths.cpp:       11/5/2009 11:38:28pm
    
    --HG--
    branch : 9 - no new

--- sources/count paths.cpp
+++ sources/count paths.cpp
@@ -2,130 +2,133 @@
 #include "cstdlib"
 
 // shared variables
-static Node *target;
+static uint *target;
 static uint columns;
+static uint D;	// depth, or something :)
 
-static void search4way(Node *current , uint steps);															// search4way
+static void search4way(uint *current , uint steps);															// search4way
 
-uint count_paths(Node *map , uint columns , uint pi , uint pj , uint ti , uint tj , uint steps)					// count_paths
+uint count_paths(uint *map , uint columns , uint pi , uint pj , uint ti , uint tj , uint steps)					// count_paths
 {	/* now steps must be less than UINT_MAX which is of course no problem almost all cases. */
 	// look for trivial instance of the problem
-	Node *current = map+pi*columns+pj;
-	uint D = current->dist;
-	if(D > steps || (steps-D) & 0x1)
+	::D = steps+1;
+	uint *current = map+(pi*columns+pj)*D;
+	uint dist = *current;
+	if(dist > steps || (steps-dist) & 0x1)
 		return 0;
 	
 	// initialize global variables
-	::target = map+ti*columns+tj;
+	::target = map+(ti*columns+tj)*D;
 	::columns = columns;
 
 	// commence search
 	if(steps)
 	{
-		Node *temp;
-		current->paths[steps] = 0;	// mark the (node,step) for entrance
+		uint *temp;
+		*(current+steps) = 0;	// mark the (node,step) for entrance
 		--steps;
 
 		if(steps)
 		{
-			temp = current-1;
-			if(temp->dist <= steps)
+			temp = current-D;
+			if(*temp <= steps)
 			{
-				if(temp->paths[steps] == UINT_MAX)
+				if(*(temp+steps) == UINT_MAX)
 					search4way(temp,steps);
-				current->paths[steps+1] += temp->paths[steps];
+				*(current+steps+1) += *(temp+steps);
 			} // end if
 
 
-			temp = current+1;
-			if(temp->dist <= steps)
+			temp = current+D;
+			if(*temp <= steps)
 			{
-				if(temp->paths[steps] == UINT_MAX)
+				if(*(temp+steps) == UINT_MAX)
 					search4way(temp,steps);
-				current->paths[steps+1] += temp->paths[steps];
+				*(current+steps+1) += *(temp+steps);
 			} // end if
 
-			temp = current-columns;
-			if(temp->dist <= steps)
+			temp = current-columns*D;
+			if(*temp <= steps)
 			{
-				if(temp->paths[steps] == UINT_MAX)
+				if(*(temp+steps) == UINT_MAX)
 					search4way(temp,steps);
-				current->paths[steps+1] += temp->paths[steps];
+				*(current+steps+1) += *(temp+steps);
 			} // end if
 
-			temp = current+columns;
-			if(temp->dist <= steps)
+			temp = current+columns*D;
+			if(*temp <= steps)
 			{
-				if(temp->paths[steps] == UINT_MAX)
+				if(*(temp+steps) == UINT_MAX)
 					search4way(temp,steps);
-				current->paths[steps+1] += temp->paths[steps];
+				*(current+steps+1) += *(temp+steps);
 			} // end if
 		} // end if
 		else
 		{
-			if(current-1 == target || current+1 == target || // there is only 1 target so only
-				current-columns == target || current+columns == target) // one of these can be true
+			if(current-D == target || current+D == target || // there is only 1 target so only
+				current-columns*D == target || current+columns*D == target) // one of these can be true
 			{
-				current->paths[1] = 1;
+				*(current+1) = 1;
 			} // end if				
 		} // end else
-		return current->paths[steps+1];
+		return *(current+steps+1);
 	} // end if
 	else if(current == target)
 	{
-		current->paths[steps] = 1;
-		return current->paths[steps];
+		//*(current+0) = 1;
+		//return *(current+0);
+		return 1;
 	} // end else
 } // end function count_paths
 
 
-static void search4way(Node *current , uint steps)															// search4way
+static void search4way(uint *current , uint steps)															// search4way
 {	/* 0 < steps < UINT_MAX *current not an obstacle and not too far */
-	Node *temp;
-	current->paths[steps] = 0;	// mark the (node,step) for entrance
+	uint *temp;
+	*(current+steps) = 0;	// mark the (node,step) for entrance
 	--steps;
 
 	if(steps)
 	{
-		temp = current-1;
-		if(temp->dist <= steps)
+		temp = current-D;
+		if(*temp <= steps)
 		{
-			if(temp->paths[steps] == UINT_MAX)
+			if(*(temp+steps) == UINT_MAX)
 				search4way(temp,steps);
-			current->paths[steps+1] += temp->paths[steps];
+			*(current+steps+1) += *(temp+steps);
 		} // end if
 
 
-		temp = current+1;
-		if(temp->dist <= steps)
+		temp = current+D;
+		if(*temp <= steps)
 		{
-			if(temp->paths[steps] == UINT_MAX)
+			if(*(temp+steps) == UINT_MAX)
 				search4way(temp,steps);
-			current->paths[steps+1] += temp->paths[steps];
+			*(current+steps+1) += *(temp+steps);
 		} // end if
 
-		temp = current-columns;
-		if(temp->dist <= steps)
+		temp = current-columns*D;
+		if(*temp <= steps)
 		{
-			if(temp->paths[steps] == UINT_MAX)
+			if(*(temp+steps) == UINT_MAX)
 				search4way(temp,steps);
-			current->paths[steps+1] += temp->paths[steps];
+			*(current+steps+1) += *(temp+steps);
 		} // end if
 
-		temp = current+columns;
-		if(temp->dist <= steps)
+		temp = current+columns*D;
+		if(*temp <= steps)
 		{
-			if(temp->paths[steps] == UINT_MAX)
+			if(*(temp+steps) == UINT_MAX)
 				search4way(temp,steps);
-			current->paths[steps+1] += temp->paths[steps];
+			*(current+steps+1) += *(temp+steps);
 		} // end if
 	} // end if
 	else
 	{
-		if(current-1 == target || current+1 == target || // there is only 1 target so only
-			current-columns == target || current+columns == target) // one of these can be true
+		if(current-D == target || current+D == target || // there is only 1 target so only
+			current-columns*D == target || current+columns*D == target) // one of these can be true
 		{
-			current->paths[1] = 1;
+			*(current+1) = 1;
 		} // end if				
 	} // end else
 } // end function search4way
--- sources/count paths.h
+++ sources/count paths.h
@@ -4,15 +4,8 @@
 // typedefs
 typedef unsigned int uint;
 
-// types
-struct Node
-{
-	uint dist;
-	uint *paths;
-}; // end struct Node
-
 
 // prototypes
-uint count_paths(Node *map , uint columns , uint pi , uint pj , uint ti , uint tj , uint steps);					// count_paths
+uint count_paths(uint *map , uint columns , uint pi , uint pj , uint ti , uint tj , uint steps);					// count_paths
 
 #endif // COUNT_PATHS_H
